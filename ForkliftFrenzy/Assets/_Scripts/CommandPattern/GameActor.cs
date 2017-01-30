ac45469fe560b1f6f033a9ba7a39133fec5df3b7
ZOE <40182161@live.napier.ac.uk>
2017-01-30 22:53:48 +0000

    dinky added, physics improved

--- ForkliftFrenzy/Assets/_Scripts/CommandPattern/GameActor.cs
+++ ForkliftFrenzy/Assets/_Scripts/CommandPattern/GameActor.cs
@@ -1,33 +1,78 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+enum forkType { redneck, dinky };
+
 public class GameActor : MonoBehaviour
 {
     public float speed = 2.0f;
     public float forkSpeed = 2.0f;
 
     private GameObject fork;
+    private GameObject upperBound;     // game object reference to highest point
     private GameObject[] backWheels;
     private GameObject[] frontWheels;
+    private Light[] breakLights = new Light[2];
 
     public float turnSpeed = 2.0f;
 
     private Vector3 offset;
+    private Vector3 offsetY;
+
+    public Vector3 com;
+    public Rigidbody rb;
+
+    // temp will change
+    public GameObject red;
+    public GameObject dink;
+
+    private int forkliftChoice = 0;
 
     // Initialise fork object
     void Start()
     {
         fork = GameObject.FindGameObjectWithTag("Fork");
+        upperBound = GameObject.Find("UpperBound");
         backWheels = GameObject.FindGameObjectsWithTag("backWheels");
         frontWheels = GameObject.FindGameObjectsWithTag("frontWheels");
 
         // camera offset 
         offset = new Vector3(transform.position.x, transform.position.y + 8.0f, transform.position.z + 7.0f);
+        offsetY = new Vector3(transform.position.x, transform.position.y, transform.position.z + 7.0f);
+
+        BreakLightsInit();
     }
 
     void Update()
     {
+        rb.centerOfMass = com;
         CameraRotation();
+
+        // Temp test will change
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            forkliftChoice ^= 1;
+        }
+
+        ChangeFork();
+    }
+
+    void ChangeFork()
+    {
+        switch((forkType)forkliftChoice)
+        {
+            case forkType.redneck:
+                dink.SetActive(false);
+                red.SetActive(true);
+                
+                break;
+
+            case forkType.dinky:
+                dink.SetActive(true);
+                red.SetActive(false);
+
+                break;
+        }
     }
 
     public void Drive(float direction)
@@ -90,7 +135,7 @@ public class GameActor : MonoBehaviour
     public void LowerForks()
     {
         // constraint
-        if (fork.transform.localPosition.y < -0.5f)
+        if (fork.transform.localPosition.y < 0.0f)
             return;
 
         Vector3 translation = new Vector3(0.0f, -0.1f, 0.0f);
@@ -101,7 +146,7 @@ public class GameActor : MonoBehaviour
     public void RaiseForks()
     {
         // constraint
-        if (fork.transform.localPosition.y > 2.3f)
+        if (fork.transform.localPosition.y > upperBound.transform.localPosition.y)
             return;
 
         Vector3 translation = new Vector3(0.0f, 0.1f, 0.0f);
@@ -112,6 +157,18 @@ public class GameActor : MonoBehaviour
     public void Ability()
     {
 
+    }
+
+    private void BreakLightsInit()
+    {
+        GameObject[] lightGO = GameObject.FindGameObjectsWithTag("breakLights");
+
+        for (int i = 0; i < lightGO.Length; ++i)
+        {
+            // get reference to each light then set to off
+            breakLights[i] = lightGO[i].GetComponent<Light>();
+            breakLights[i].enabled = false;
+        }
     }
 
     public void CameraRotation()
@@ -125,7 +182,8 @@ public class GameActor : MonoBehaviour
     {
         // transform x asis
         offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
-        Camera.main.transform.position = transform.position + offset;
+        //offsetY = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * turnSpeed, Vector3.right) * offsetY;
+        Camera.main.transform.position = transform.position + offset;// + offsetY;
         Camera.main.transform.LookAt(transform.position);
     }
 
