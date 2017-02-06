ee5a1d82cab4d0e98b29b406ebee7a0a475bfa1d
Matt <mrnewbigging@hotmail.com>
2017-02-06 16:15:47 +0000

    Drop off code completed

---
+++ ForkliftFrenzy/Assets/_Scripts/DestroyBoxZone.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyBoxZone : MonoBehaviour {
+
+	void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.tag == "Pick-Up")
+        {
+            // Send info to GM (how to access properly?)
+            // GM.SendBox(col.gameObject);
+
+            // Destroy box object
+            Destroy(col.gameObject);
+        }
+    }
+}
