8e9e5d71cb458ab7861d198f8c79484f04e63464
ZOE <40182161@live.napier.ac.uk>
2017-02-07 15:11:45 +0000

    Timer and scoring system with box spawning initial test working

--- ForkliftFrenzy/Assets/_Scripts/DestroyBoxZone.cs
+++ ForkliftFrenzy/Assets/_Scripts/DestroyBoxZone.cs
@@ -3,12 +3,14 @@ using System.Collections;
 
 public class DestroyBoxZone : MonoBehaviour {
 
+    public GameManager GM;
+
 	void OnTriggerStay(Collider col)
     {
         if (col.gameObject.tag == "Pick-Up")
         {
             // Send info to GM (how to access properly?)
-            // GM.SendBox(col.gameObject);
+            GM.SendBox(col.gameObject);
 
             // Destroy box object
             Destroy(col.gameObject);
