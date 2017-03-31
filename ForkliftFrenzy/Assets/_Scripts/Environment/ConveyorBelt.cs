0dc415f5a7bc2f6e2ff31b3a202e14eed88ea8f7
Zoe Wall <zoe@zwall.co.uk>
2017-03-31 14:51:33 +0100

    Cleaned dir

--- ForkliftFrenzy/Assets/_Scripts/Environment/ConveyorBelt.cs
+++ ForkliftFrenzy/Assets/_Scripts/Environment/ConveyorBelt.cs
@@ -14,20 +14,11 @@ public class ConveyorBelt : MonoBehaviour {
     // Moves pick-ups along belt at speed
     void OnTriggerStay(Collider col)
     {
+        // added new trigger. Need 
 
         if (col.isTrigger)
         {
             col.transform.position =  Vector3.MoveTowards(col.transform.position, beltEnd.position, speed * Time.deltaTime);
-        }
-
-
-        // Only want to move 
-        //if (col.transform.parent.tag == "Pick-up")// && col.transform.parent.tag != "Fork")
-        {
-           // Debug.Log(col.transform.parent.tag);
-
-            // Move pick-up towards end point       
-         //   col.gameObject.transform.position = Vector3.MoveTowards(col.gameObject.transform.position, beltEnd.position, speed * Time.deltaTime);
         }
     }
        
